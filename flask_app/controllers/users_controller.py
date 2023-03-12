a298792618a5816c1b85b3dc67f8a370284c6dd4
JocelynMira <mkoshiba02@gmail.com>
2023-03-12 11:27:59 -1000

    initial commit

---
+++ flask_app/controllers/users_controller.py
@@ -0,0 +1,77 @@
+from flask_app import app
+from flask import render_template, redirect, request, flash, session
+from flask_app.models.user_model import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt (app)
+
+# route to main page
+@app.route('/')
+def index():
+    # check if user is logged in
+    if 'user_id' not in session:
+        return render_template ('register.html')
+    else:
+        return redirect ('/dashboard')
+
+# save new user
+@app.route('/register', methods = ['POST'])
+def save():
+    # if user is not validated, flash messages and redirect back
+    if not User.validate_registration(request.form):
+        return redirect ('/')
+    # if user validated, create data object so you can hash password and save user to db
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        # this is where we hash password
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.save(data)
+    # just to check for errors (mostly errors related to bcrypt)
+    if not id:
+        flash('Something went wrong')
+    else:
+        session['user_id'] = id
+        # put the user_id into session so we can keep track of logged in user
+        flash("Success! You're now registered")
+        return redirect ('/dashboard')
+
+# route to dashboard/welcome page 
+@app.route('/dashboard')
+def dashboard():
+    if "user_id" not in session:
+        return redirect ('/')
+    data = {
+        'id' : session['user_id']
+    }
+    return render_template ('dashboard.html' , user = User.get_user_by_id(data)) # left html side, right controller side
+
+# user login
+# needs to be a POST because of bcrypt function
+@app.route('/login', methods = ['POST'])
+def login():
+    data = {
+        'email' : request.form['email']
+    }
+    # check if the email is in database
+    user = User.get_user_by_email(data)
+    if not user:
+        # check user email
+        flash('Email not found. Please register.', "login")
+        return redirect ('/')
+        # compare password to hashed password
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash('Invalid password', "login")
+        return redirect ('/')
+    else:
+        session['user_id'] = user.id
+        flash('You are now logged in')
+        return redirect ('/dashboard')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("You're now logged out.")
+    return redirect ('/')
---
+++ flask_app/models/user_model.py
@@ -0,0 +1,96 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import app
+from flask import flash
+import re #regex101.com
+# import time (this is for dates/time)
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+    db = "user_registration"
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    # CREATE
+    @classmethod
+    def save(cls, data):
+        query = """INSERT INTO users (first_name, last_name, email, password)
+                VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s )
+                """
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return results
+
+    # READ
+    @classmethod
+    def show_all(cls):
+        query = "SELECT * FROM users"
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
+    
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = """SELECT * FROM users
+                WHERE email = %(email)s;
+                """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        #results would be in a list 
+        return cls(results[0])
+    
+    @classmethod
+    def get_user_by_id(cls, data):
+        query = """SELECT * FROM users
+                WHERE id = %(id)s;
+                """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        return cls(results[0])
+
+
+    # DELETE
+    @classmethod
+    def delete(cls, id):
+        query = """DELETE FROM users 
+                WHERE id = %(id)s;"""
+        results = connectToMySQL(cls.db).query_db(query, {'id': id})
+        return results
+    
+    @staticmethod
+    def validate_registration(user):
+        is_valid = True
+        # REGISTER VALIDATIONS
+        # checking database to make sure they're not already registered 
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        results = connectToMySQL(User.db).query_db(query, user)
+        if len(results) >= 1:
+            flash ("Email already registered. Please log in!", "register")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Invalid email format!', "register")
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash ('First Name must be at least 2 characters.', "register")
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash ('Last Name must be at least 2 characters.', "register")
+            is_valid = False
+        if len(user['password']) < 8:
+            flash ('Password must be at least 8 characters.', "register")
+            is_valid = False
+        # compare password
+        if user['password'] != user['confirm_password']:
+            flash("Passwords do not match", "register")
+            is_valid = False
+        return is_valid
